629677c1dcd6098cf80cf6b67088f4424aaf7fd3
Rajaonarison Alain Patrick <Rajaonarison Alain Patrick>
2016-10-22 12:42:32 +0300

    Ny Baiboliko v1.6

--- app/src/main/java/com/patrick/developer/nybaiboliko/configuration/SqliteHelper.java
+++ app/src/main/java/com/patrick/developer/nybaiboliko/configuration/SqliteHelper.java
@@ -21,7 +21,7 @@ import java.sql.SQLException;
 
 public class SqliteHelper extends OrmLiteSqliteOpenHelper {
     protected static final String DATABASE_NAME = "bible";
-    protected static final int DATABASE_VERSION = 2;
+    protected static final int DATABASE_VERSION = 3;
 
     public SqliteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
